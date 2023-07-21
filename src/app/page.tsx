fa17fdec6a03dd61aa996e1a8044762fdd7392ca
Dmitriy S <dmitrsold@gmail.com>
2023-07-21 12:35:39 +0300

    alpha version done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,207 @@
-import Image from 'next/image'
+"use client";
+import Select, { MultiValue, SingleValue, StylesConfig } from "react-select";
+import AsyncSelect from "react-select/async";
+import CreatableSelect from "react-select/creatable";
+import { useForm, Controller } from "react-hook-form";
+import { useEffect, useState } from "react";
+
+const DEPS = ["DEV", "QA"] as const;
+const ISSUE_TYPES_BY_DEP: Record<string, string[]> = {
+  DEV: ["ERROR", "WARNING", "INFO"],
+  QA: ["UI", "FUNCTIONAL"],
+};
+const DESC_TEMPLATE: Record<string, string> = {
+  ERROR: "My error is:",
+  WARNING: "My warning is:",
+  // INFO: "My info is:",
+  UI: "My UI issue is:",
+  FUNCTIONAL: "My functional issue is:",
+};
+
+const sleep = (ms = 1000) => new Promise((resolve) => setTimeout(resolve, ms));
+
+async function loadDeps() {
+  await sleep();
+  return DEPS;
+}
+
+async function loadIssueTypesByDep(dep: string) {
+  await sleep();
+  return ISSUE_TYPES_BY_DEP[dep].map((iss) => ({
+    type: iss,
+    template: DESC_TEMPLATE[iss],
+  }));
+}
+
+type Issue = Awaited<ReturnType<typeof loadIssueTypesByDep>>[number];
+type Option = { label: string; value: string };
+type IssueOption = Option & Pick<Issue, "template">;
+
+type FormValues = {
+  dep: SingleValue<Option>;
+  issueType: SingleValue<IssueOption>;
+  desc: string;
+  cc: MultiValue<Option>;
+};
+
+const selectStyles = {
+  option: (baseStyles) => ({
+    ...baseStyles,
+    textTransform: "capitalize",
+  }),
+  valueContainer: (baseStyles) => ({
+    ...baseStyles,
+    textTransform: "capitalize",
+  }),
+} satisfies StylesConfig<Option>;
 
 export default function Home() {
+  const {
+    control,
+    register,
+    watch,
+    setValue,
+    formState: { isValid, errors },
+  } = useForm<FormValues>({
+    defaultValues: { dep: null, issueType: null, desc: "", cc: [] },
+    mode: "onBlur",
+  });
+
+  const [issueTypes, setIssueTypes] = useState<IssueOption[]>([]);
+
+  const depLive = watch("dep")?.value;
+  const descLive = watch("desc");
+  const issueTypeLive = watch("issueType");
+
+  useEffect(() => {
+    // TODO fetch in more elegant way (RQ/SWR/RSC, etc)
+    if (!depLive) return;
+
+    loadIssueTypesByDep(depLive)
+      .then((isstypes) =>
+        isstypes.map((iss) => ({
+          label: iss.type.toLowerCase(),
+          value: iss.type,
+          template: iss.template,
+        }))
+      )
+      .then(setIssueTypes);
+  }, [depLive]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex min-h-screen flex-col items-center justify-center container">
+      <form>
+        <section>
+          <Controller
+            name="dep"
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <AsyncSelect
+                {...field}
+                styles={selectStyles}
+                placeholder="Choose department"
+                className="w-72"
+                cacheOptions
+                defaultOptions
+                isSearchable={false}
+                // TODO sort by A-Z
+                loadOptions={() =>
+                  loadDeps().then((deps) =>
+                    deps.map((d) => ({ label: d.toLowerCase(), value: d }))
+                  )
+                }
+              />
+            )}
+          />
+          {errors.dep && (
+            <span className="text-red-500" role="alert">
+              This field is required
+            </span>
+          )}
+        </section>
+        <br />
+        <section>
+          <Controller
+            name="issueType"
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <Select
+                {...field}
+                onChange={(newValue) => {
+                  field.onChange(newValue);
+                  setValue("desc", newValue?.template ?? "");
+                }}
+                styles={selectStyles}
+                placeholder="Choose issue type"
+                className="w-72"
+                isDisabled={!depLive}
+                isLoading={false}
+                isSearchable={false}
+                // TODO sort by A-Z
+                options={issueTypes}
+              />
+            )}
+          />
+          {errors.issueType && (
+            <span className="text-red-500" role="alert">
+              This field is required
+            </span>
+          )}
+        </section>
+        <br />
+        <section>
+          <textarea
+            {...register("desc", { required: true })}
+            cols={28}
+            rows={5}
+            maxLength={255}
+            className="rounded p-4"
+            placeholder={
+              issueTypeLive?.template ? "" : "Enter your description"
+            }
+          />
+          <div className="text-right">{descLive.length} / 255</div>
+          {!!issueTypeLive?.template && (
+            <div className="w-72">
+              Please provide as many details as per provided template.
+            </div>
+          )}
+          {errors.desc && (
+            <span className="text-red-500" role="alert">
+              This field is required
+            </span>
+          )}
+        </section>
+        <br />
+        <section>
+          <Controller
+            name="cc"
+            control={control}
+            render={({ field }) => (
+              // TODO add email validation (in onCreateOption?)
+              <CreatableSelect
+                {...field}
+                isMulti
+                isClearable
+                placeholder="Enter your emails"
+                className="w-72"
+              />
+            )}
+          />
+        </section>
+        <br />
+        <section>
+          <button
+            className="rounded bg-blue-500 text-white px-4 py-2 hover:bg-blue-600 disabled:bg-blue-300 disabled:cursor-not-allowed"
+            disabled={!isValid}
+            type="submit"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+            Submit
+          </button>
+        </section>
+      </form>
     </main>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
